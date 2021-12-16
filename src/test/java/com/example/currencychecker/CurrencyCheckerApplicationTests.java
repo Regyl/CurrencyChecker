7c27dd81d5ab02eb0649770e22931635fdc1c79b
Regyl <novikovevgeny1@gmail.com>
2021-12-16 16:09:41 +0300

    initial commit

---
+++ src/test/java/com/example/currencychecker/CurrencyCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
