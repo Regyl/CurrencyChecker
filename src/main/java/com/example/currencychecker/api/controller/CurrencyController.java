292411ba1e03a7b0c3f0ac9a689bffa241e9a345
Regyl <novikovevgeny1@gmail.com>
2021-12-19 21:29:00 +0300

    delete useless deserializer

--- src/main/java/com/example/currencychecker/api/controller/CurrencyController.java
+++ src/main/java/com/example/currencychecker/api/controller/CurrencyController.java
@@ -1,19 +1,13 @@
 package com.example.currencychecker.api.controller;
 
 import com.example.currencychecker.api.controller.dto.response.OpenexchangeDtoResponse;
-import com.example.currencychecker.config.MapDeserializer;
 import com.example.currencychecker.service.CurrencyService;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
-import java.util.List;
-import java.util.Map;
 
 @Tag(name = "Currency")
 
--- src/main/java/com/example/currencychecker/api/controller/dto/response/OpenexchangeDtoResponse.java
+++ src/main/java/com/example/currencychecker/api/controller/dto/response/OpenexchangeDtoResponse.java
@@ -1,16 +1,8 @@
 package com.example.currencychecker.api.controller.dto.response;
 
-import com.example.currencychecker.config.MapDeserializer;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
-import org.apache.commons.lang3.tuple.ImmutablePair;
 
-import javax.validation.constraints.NotNull;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Data
 public class OpenexchangeDtoResponse {
