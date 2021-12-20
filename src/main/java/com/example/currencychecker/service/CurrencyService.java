5c6d32af023ddd76d1579fa1ebc8cf1053d93a4e
Regyl <novikovevgeny1@gmail.com>
2021-12-20 09:27:23 +0300

    finish integration with openexchange

--- src/main/java/com/example/currencychecker/service/CurrencyService.java
+++ src/main/java/com/example/currencychecker/service/CurrencyService.java
@@ -1,22 +1,24 @@
 package com.example.currencychecker.service;
 
-import com.example.currencychecker.controller.dto.response.OpenexchangeDtoResponse;
 import com.example.currencychecker.exception.CurrencyNotFoundException;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 @Service
-@Log
 public class CurrencyService {
 
+    //Вынесена по требованиям в настройки, но не используется, т.к. нужна платная подписка на openexchange
+    private final String baseCurrency;
     private final OpenexchangeClient openexchangeClient;
 
-    public CurrencyService(OpenexchangeClient openexchangeClient) {
+    public CurrencyService(OpenexchangeClient openexchangeClient,
+                           @Value("${openexchange.base-currency}") String baseCurrency) {
         this.openexchangeClient = openexchangeClient;
+        this.baseCurrency = baseCurrency;
     }
 
     public MultipartFile getGif(String name) {
@@ -40,9 +42,6 @@ public class CurrencyService {
         Float oldValue = openexchangeClient.getHistorical(LocalDate.now().minusDays(1))
                         .getRates()
                         .get(name);
-
-        log.info("new value " + newValue.toString());
-        log.info("old value " + oldValue.toString());
 
         return newValue > oldValue;
     }
--- src/main/java/com/example/currencychecker/service/OpenexchangeClient.java
+++ src/main/java/com/example/currencychecker/service/OpenexchangeClient.java
@@ -3,10 +3,12 @@ package com.example.currencychecker.service;
 import com.example.currencychecker.config.OpenexchangeConfiguration;
 import com.example.currencychecker.controller.dto.response.OpenexchangeDtoResponse;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 
